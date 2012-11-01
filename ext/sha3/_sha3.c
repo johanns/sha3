c6d4e92e1fde0866316b2a5be06ea23d7e8d4fd6
Johanns Gregorian <io.johannsg+git@gmail.com>
2012-11-01 12:35:13 -0700

    Initial commit for SHA3::Digest, a Ruby::Digest derived class - Skeleton functions overwrite (initialize, alloc, update, reset, copy, …) - Added DigestError, a StandardError derived class. - Updated digest function to use DigestError instead.

--- ext/sha3/_sha3.c
+++ ext/sha3/_sha3.c
@@ -1,9 +1,6 @@
-#include <ruby.h>
-#include "KeccakNISTInterface.h"
+#include "_sha3.h"
 
-#define MAX_DIGEST_SIZE 64
-
-VALUE mSHA3;
+static VALUE c_digest_update(VALUE, VALUE);
 
 /* @overload digest(data, data_len, hashbit_len)
  * 
@@ -18,21 +15,52 @@ VALUE m_sha3_digest(VALUE self, VALUE data, VALUE datalen, VALUE hashbitlen)
   int hlen = NUM2INT(hashbitlen);
 
   if ((hlen != 224) && (hlen != 256) && (hlen != 384) && (hlen != 512))
-    rb_raise(rb_eArgError, "invalid hashbit_len given (valid options: 224, 256, 384, or 512)");
+    rb_raise(rb_eArgError, "invalid hashbit_len given (valid options are: 224, 256, 384, and 512)");
 
   StringValue(data);
 
   VALUE s = rb_str_new(0, hlen / 8);
 
   if (Hash(hlen, RSTRING_PTR(data), NUM2ULL(datalen), RSTRING_PTR(s)) != SUCCESS)
-    rb_raise(rb_eException, "unable to generate hash");
+    rb_raise(eDigestError, "failed to generate hash");
 
   return s;
 }
 
+static VALUE c_digest_alloc(VALUE klass) {}
+static VALUE c_digest_init(int argc, VALUE *argv, VALUE self) {}
+static VALUE c_digest_update(VALUE self, VALUE data) {}
+static VALUE c_digest_reset(VALUE self) {}
+static VALUE c_digest_copy(VALUE self, VALUE obj) {}
+static VALUE c_digest_length(VALUE self) {}
+static VALUE c_digest_block_length(VALUE self) {}
+static VALUE c_digest_name(VALUE self) {}
+static VALUE c_digest_finish(int argc, VALUE *argv, VALUE self) {}
+
 void Init_sha3_n()
 {
-  mSHA3 = rb_define_module("SHA3");
+  rb_require("digest");
 
+  // SHA3 (module)
+  mSHA3 = rb_define_module("SHA3");
+  // SHA3::Digest (class)
+  cDigest = rb_define_class_under(mSHA3, "Digest", rb_path2class("Digest::Class"));
+  // SHA3::Digest::DigestError (class)
+  eDigestError = rb_define_class_under(cDigest, "DigestError", rb_eStandardError);
+
+  // SHA3 (module) functions
   rb_define_module_function(mSHA3, "digest", m_sha3_digest, 3);
+
+  // SHA3::Digest (class) methods
+  rb_define_alloc_function(cDigest, c_digest_alloc);
+  rb_define_method(cDigest, "initialize", c_digest_init, -1);
+  rb_define_method(cDigest, "update", c_digest_update, 1);
+  rb_define_method(cDigest, "reset", c_digest_reset, 0);
+  rb_define_method(cDigest, "initialize_copy", c_digest_copy, 1);
+  rb_define_method(cDigest, "digest_length", c_digest_length, 0);
+  rb_define_method(cDigest, "block_legnth", c_digest_block_length, 0);
+  rb_define_method(cDigest, "name", c_digest_name, 0);
+  rb_define_private_method(cDigest, "finish", c_digest_finish, -1);
+
+  return;
 }
---
+++ ext/sha3/_sha3.h
@@ -0,0 +1,11 @@
+#ifndef __SHA3_H_
+#define __SHA3_H_
+
+#include <ruby.h>
+#include "KeccakNISTInterface.h"
+
+VALUE mSHA3;
+VALUE cDigest;
+VALUE eDigestError;
+
+#endif
