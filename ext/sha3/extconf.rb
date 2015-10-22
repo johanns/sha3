3db0723e98cd8fb56cfea94bc4dce820ad93a707
Johanns Gregorian <johanns@fingerprintplay.com>
2015-10-21 23:34:54 -0700

    extconf.rb: Updated to use new Keccak code

--- ext/sha3/extconf.rb
+++ ext/sha3/extconf.rb
@@ -1,24 +1,19 @@
 require 'mkmf'
 require 'rbconfig'
 
-FileUtils.rm "#{$srcdir}/KeccakF-1600-opt.c", :force => true
-
 target_cpu = RbConfig::CONFIG['target_cpu']
 
 if 1.size == 4 and target_cpu =~ /i386|x86_32/   # x86 32bit optimized code
-  Logging::message "=== Using i386 optimized Keccak code ===\n"
-  FileUtils.cp "#{$srcdir}/KeccakF-1600-opt32.c-arch", "#{$srcdir}/KeccakF-1600-opt.c"
+  Logging::message "=== Using reference  ===\n"
+  FileUtils.cp "#{$srcdir}/Reference/*.*", "#{$srcdir}/"
 elsif 1.size == 8 and target_cpu =~ /i686|x86_64/
-  Logging::message "=== Using i686 optimized Keccak code ===\n"
-  FileUtils.cp "#{$srcdir}/KeccakF-1600-opt64.c-arch", "#{$srcdir}/KeccakF-1600-opt.c"
-else # Ha? Use reference code -- slow
-  Logging::message "=== Using reference Keccak code ===\n"
-  FileUtils.cp "#{$srcdir}/KeccakF-1600-reference.c-arch", "#{$srcdir}/KeccakF-1600-opt.c"
+  Logging::message "=== Using optimized (64-bit) ===\n"
+  FileUtils.cp Dir["#{$srcdir}/Optimized64/*"].collect{ |f| File.expand_path(f) }, "#{$srcdir}/"
+else
+  Logging::message "=== Using reference ===\n"
+  FileUtils.cp Dir["#{$srcdir}/Reference/*"].collect{ |f| File.expand_path(f) }, "#{$srcdir}/"
 end
 
-find_header("KeccakF-1600-interface.h")
-find_header("KeccakSponge.h")
-find_header("KeccakNISTInterface.h")
 find_header("sha3.h")
 find_header("digest.h")
 
