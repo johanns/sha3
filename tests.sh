751575c324b4f773aa1e21ffa5f57df597841f3a
Johanns Gregorian <io@jsg.io>
2020-12-01 23:35:04 -0800

    test.sh: Updated test vectors file URLs

--- tests.sh
+++ tests.sh
@@ -5,22 +5,22 @@ rm -fv spec/sha3_digest*.rb
 
 if [ -d "spec/data" ]
 then
-  rm -rfv spec/data/*
+    rm -rfv spec/data/*
 else
-  mkdir "spec/data"
+    mkdir "spec/data"
 fi
 
 pushd "spec/data"
 
 if [ -f "*.txt" ]
 then
-  rm -v *.txt
+    rm -v *.txt
 fi
 
-wget "https://raw.githubusercontent.com/gvanas/KeccakCodePackage/master/TestVectors/ShortMsgKAT_SHA3-224.txt"
-wget "https://raw.githubusercontent.com/gvanas/KeccakCodePackage/master/TestVectors/ShortMsgKAT_SHA3-256.txt"
-wget "https://raw.githubusercontent.com/gvanas/KeccakCodePackage/master/TestVectors/ShortMsgKAT_SHA3-384.txt"
-wget "https://raw.githubusercontent.com/gvanas/KeccakCodePackage/master/TestVectors/ShortMsgKAT_SHA3-512.txt"
+wget "https://raw.githubusercontent.com/XKCP/XKCP/master/tests/TestVectors/ShortMsgKAT_SHA3-224.txt"
+wget "https://raw.githubusercontent.com/XKCP/XKCP/master/tests/TestVectors/ShortMsgKAT_SHA3-256.txt"
+wget "https://raw.githubusercontent.com/XKCP/XKCP/master/tests/TestVectors/ShortMsgKAT_SHA3-384.txt"
+wget "https://raw.githubusercontent.com/XKCP/XKCP/master/tests/TestVectors/ShortMsgKAT_SHA3-512.txt"
 
 cd ".."
 
