125765f1284279b5cc56113c26b94af26411e318
Johanns Gregorian <io@jsg.io>
2020-12-03 23:33:08 -0800

    sha3/version: Bumped version to 1.0.2

--- lib/sha3/version.rb
+++ lib/sha3/version.rb
@@ -2,9 +2,8 @@ module SHA3
   extend self
 
   # sha3 release version
-  VERSION = "1.0.1"
+  VERSION = '1.0.2'.freeze
 
   # keccak version number
-  KECCAK_VERSION = "4.0"
+  KECCAK_VERSION = '4.0'.freeze
 end
-
