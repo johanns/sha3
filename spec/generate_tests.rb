75b9d606e3c615e7483905e1533afa3395014aef
Johanns Gregorian <io@jsg.io>
2025-02-26 18:33:18 -0800

    refactor: improve test vector generation script for SHA3 digest tests

--- spec/generate_tests.rb
+++ spec/generate_tests.rb
@@ -9,71 +9,77 @@ FILES = [
   ['data/ShortMsgKAT_SHA3-512.txt', 512]
 ].freeze
 
-# rubocop:disable Layout/HeredocIndentation(RuboCop)
-REQUIRED = <<-REQUIRED
-# frozen_string_literal: true
+TEMPLATES = {
+  required: <<~REQUIRED,
+    # frozen_string_literal: true
 
-require 'spec_helper'
-require 'sha3'
-REQUIRED
+    require 'spec_helper'
+    require 'sha3'
+  REQUIRED
 
-SPEC = <<-SPEC
+  spec: <<~SPEC,
 
-RSpec.describe 'SHA3::Digest.new(HASH_LEN)' do
-  it 'should pass byte-length test vectors of SHA3-HASH_LEN (NAME)' do
-SPEC
+    RSpec.describe 'SHA3::Digest.new(HASH_LEN)' do
+      it 'should pass byte-length test vectors of SHA3-HASH_LEN (NAME)' do
+  SPEC
 
-EXPECT = <<-EXPECT
+  expect: <<~EXPECT,
     expect(SHA3::Digest.new(HASHLEN, ['MSG'].pack('H*')).hexdigest).to eq('DIGEST')
-EXPECT
+  EXPECT
 
-ENDING = <<~ENDING
-  end
+  ending: <<~ENDING
+      end
+    end
+
+  ENDING
+}.freeze
+
+def parse_test_file(path)
+  File.read(path)
+      .split('Len = ')
+      .filter_map do |test|
+        lines = test.split("\n")
+        next if lines.empty? || lines[0] =~ /^#/
+
+        length = lines[0].to_i
+        next unless (length % 8).zero? && length.positive?
+
+        msg_raw = lines[1].split(' = ').last
+        digest = lines[2].split(' = ').last.downcase
+
+        [msg_raw, digest]
+      end
 end
 
-ENDING
-# rubocop:enable Layout/HeredocIndentation(RuboCop)
+def generate_test_file(path, hashlen)
+  name = File.basename(path).split('.')[0]
+  filename = "sha3_digest_#{hashlen}_spec.rb"
 
-# rubocop:disable Metrics/AbcSize(RuboCop) # (RuboCop)
-# rubocop:disable Metrics/MethodLength(RuboCop)
-def gen_digest_byte_tests
-  FILES.each do |path, hashlen|
-    name = File.basename(path).split('.')[0]
-
-    f = File.new("sha3_digest_#{hashlen}_spec.rb", 'w')
-    f.puts(REQUIRED)
+  File.open(filename, 'w') do |f|
+    f.puts(TEMPLATES[:required])
     f.puts(
-      SPEC
-      .gsub(/HASH_LEN/, hashlen.to_s)
-      .gsub(/NAME/, name)
+      TEMPLATES[:spec]
+        .gsub('HASH_LEN', hashlen.to_s)
+        .gsub('NAME', name)
     )
 
-    contents = File.read(path).split('Len = ')
-    contents.each do |test|
-      lines = test.split("\n")
-      next unless !lines.empty? && lines[0] !~ /^#/
-
-      length = lines[0].to_i
-      next unless (length % 8).zero? && length != 0
-
-      msg_raw = lines[1].split(' = ').last
-      digest = lines[2].split(' = ').last.downcase
-
+    parse_test_file(path).each do |msg_raw, digest|
       f.puts(
-        EXPECT
-          .gsub(/HASHLEN/, hashlen.to_s)
-          .gsub(/MSG/, msg_raw)
-          .gsub(/DIGEST/, digest)
+        TEMPLATES[:expect]
+          .gsub('HASHLEN', hashlen.to_s)
+          .gsub('MSG', msg_raw)
+          .gsub('DIGEST', digest)
       )
     end
 
-    f.puts(ENDING)
-    f.close
+    f.puts(TEMPLATES[:ending])
   end
 end
-# rubocop:enable Metrics/AbcSize(RuboCop)
-# rubocop:enable Metrics/MethodLength(RuboCop)
 
-def setup; end
+def gen_digest_byte_tests
+  FILES.each do |path, hashlen|
+    generate_test_file(path, hashlen)
+  end
+end
 
 gen_digest_byte_tests
